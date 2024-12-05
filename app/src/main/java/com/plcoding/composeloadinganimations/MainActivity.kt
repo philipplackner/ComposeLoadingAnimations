f11dd67e3f20b177c2f64e15704ac1a0a7bd4939
Philipp Lackner <mail@pl-coding.com>
2024-12-05 13:10:23 +0100

    Initial commit

---
+++ app/src/main/java/com/plcoding/composeloadinganimations/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.plcoding.composeloadinganimations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.composeloadinganimations.ui.theme.ComposeLoadingAnimationsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeLoadingAnimationsTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = Color.Black
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        BlurredAnimatedText(
+                            text = "Hello world!"
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
